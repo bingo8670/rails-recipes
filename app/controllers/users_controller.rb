c65fe32f7f496f592b371fd5b45639101e6d2268
bingo8670 <307286130@qq.com>
2017-06-23 21:47:32 +0800

    user can edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
-    @user = current_user
+    @user.create_profile unless @user.profile
+    # unless @user.profile 等同于 if !@user.profile 或 if @user.profile.nil?
   end
 
   def update
-    @user = current_user
+    @user.create_profile unless @user.profile
 
     if @user.update(user_params)
       flash[:notice] = "修改成功"
@@ -18,8 +23,13 @@ class UsersController < ApplicationController
 
   protected
 
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end
+
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty] )
   end
 
 end
